238ee2c9c4f983f60f1f92ddacdad431292ba61d
shivanshssgupta <sg120@snu.edu.in>
2020-11-17 02:20:25 +0530

    Initial Commit

--- lib/ui/Home/pages/dashboard.dart
+++ lib/ui/Home/pages/dashboard.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DashBoard extends StatefulWidget {
   @override
@@ -6,8 +10,180 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  Coordinates currentUserLatLong;
+  var cityName = "";
+
+  void getCurrentLocation() async {
+    final position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    currentUserLatLong = new Coordinates(position.latitude, position.longitude);
+    final address =
+        await Geocoder.local.findAddressesFromCoordinates(currentUserLatLong);
+    final first = address.first;
+    cityName = first.locality;
+    print(cityName);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                margin: EdgeInsets.only(left: 115),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.blue,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      cityName,
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600)),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0.0, 1.0), //(x,y)
+                        blurRadius: 6.0,
+                      )
+                    ]),
+                child: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                "Category",
+                style: GoogleFonts.sourceSansPro(
+                    fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                width: 85,
+                height: 110,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.elliptical(70, 60)),
+                ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.lightGreen,
+                            borderRadius: BorderRadius.circular(30)),
+                        child: IconButton(
+                            icon: Icon(
+                              Icons.hotel,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {}),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("Hotel",style: GoogleFonts.sourceSansPro(fontSize: 16,fontWeight: FontWeight.w500),)
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                width: 85,
+                height: 110,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.elliptical(70, 60)),
+                ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.indigo,
+                            borderRadius: BorderRadius.circular(30)),
+                        child: IconButton(
+                            icon: Icon(
+                              Icons.restaurant,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {}),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("Restaurant",style: GoogleFonts.sourceSansPro(fontSize: 16,fontWeight: FontWeight.w500),)
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                width: 85,
+                height: 110,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.elliptical(70, 60)),
+                ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.deepOrangeAccent,
+                            borderRadius: BorderRadius.circular(30)),
+                        child: IconButton(
+                            icon: Icon(
+                              Icons.tour,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {}),
+                      ),
+                      SizedBox(height: 5,),
+                      Text("Places",style: GoogleFonts.sourceSansPro(fontSize: 16,fontWeight: FontWeight.w500),)
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
